3e18d4ebc0e9aec666018e2847d1620e5a7f712a
Alexander Goponenko <agoponenko@gmail.com>
2020-05-18 17:18:26 -0400

    environmental variable for server

--- sevi.c
+++ sevi.c
@@ -6,37 +6,28 @@
 #include "client.h"
 
 #define MAX 1024*4
-#define PORT "9999"
 #define SA struct sockaddr
 
+static const char *REMOTE_SERVER_ENV_NAME = "VINSNL_SERVER";
+static const char *REMOTE_SERVER_STRING = "127.0.0.1:9999";
 static const char *VARIETY_ID_ENV_NAME = "LDMS_VARIETY_ID";
 static const char *JOB_ID_ENV_NAME = "SLURM_JOB_ID";
 
-extern char **environ;
 
-static void _run_sevi(int sockfd) {
 
-//  char buff[MAX+1];
+static int _run_sevi(int sockfd) {
+
+  int rc = 0;
 
   char * variety_id = getenv(VARIETY_ID_ENV_NAME);
   char * job_id = getenv(JOB_ID_ENV_NAME);
 
-  printf("Job_id: %s", job_id);
+  printf("Job_id: %s\n", job_id);
 
   cJSON *req =  cJSON_CreateObject();
   cJSON_AddStringToObject(req, "type", "process_job");
   cJSON_AddStringToObject(req, "job_id", job_id);
   cJSON_AddStringToObject(req, "variety_id", variety_id);
-
-//  int i = 1;
-//  char *s = *environ;
-//
-//  for (; s; i++) {
-//    sprintf(buff, "%i", i);
-//    cJSON_AddStringToObject(req, buff, s);
-//    printf("%s\n", s);
-//    s = *(environ+i);
-//  }
 
   cJSON *resp = send_receive(sockfd, req);
 
@@ -44,16 +35,36 @@ static void _run_sevi(int sockfd) {
 
   if (resp == NULL) {
     printf("Got Null\n");
+    rc = 33;
   } else {
-    cJSON *payload = cJSON_GetObjectItem(resp, "response");
+    cJSON *payload = cJSON_GetObjectItem(resp, "status");
     printf("Got: %s\n",payload->valuestring);
+    if (strcmp(payload->valuestring, "ACK") == 0) {
+      rc = 0;
+    } else {
+      rc = 44;
+    }
+    cJSON_Delete(resp);
   }
+  return rc;
 }
 
 
+
 int main(int argc, char *argv[]) {
-  char *addr = "127.0.0.1";
-  char *port = PORT;
+  /* initializing server address */
+  char *server_string = getenv(REMOTE_SERVER_ENV_NAME);
+    if (server_string == NULL)
+      server_string = REMOTE_SERVER_STRING;
+  char * colon = strstr(server_string, ":");
+  if (!colon) {
+    printf ("malformed sever string: \"%s\"\n", server_string);
+    exit(1);
+//<-----
+  }
+  char *addr = strndup(server_string, colon - server_string);
+  char *port = strdup(colon+1);
+
   int sockfd;
 
   if (argc > 1) {
@@ -66,17 +77,20 @@ int main(int argc, char *argv[]) {
   sockfd = connect_to_simple_server(addr, port);
 
   if (sockfd == -1) {
-    exit(1);
-// <----
+    exit(2);
+//<-----
   }
 
   printf("addr: %s ; port: %s \n", addr, port);
 
   // run the main task
-  _run_sevi(sockfd);
+  int rc = _run_sevi(sockfd);
 
   // close the socket
   close(sockfd);
+  free(addr);
+  free(port);
 
+  return rc;
 }
 
